f5fff2c315083c33efe0f0ff39fc200f5c692bbc
Ben Fortuna <benfortuna@users.noreply.github.com>
2022-12-01 04:00:33 +0000

    Support configurable contacts

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
-module "aws-secure-baseline" {
-  source = "./aws-secure-baseline"
-  count = var.aws_secure_baseline_enabled ? 1 : 0
+# module "aws-secure-baseline" {
+#   source = "./aws-secure-baseline"
+#   count = var.aws_secure_baseline_enabled ? 1 : 0
 
-  support_iam_role_principal_arns = var.aws_support_roles
-  audit_log_bucket_name = var.aws_audit_log_bucket
+#   support_iam_role_principal_arns = var.aws_support_roles
+#   audit_log_bucket_name = var.aws_audit_log_bucket
 
-  providers = {
-    aws                = aws
-    aws.ap-northeast-1 = aws.ap-northeast-1
-    aws.ap-northeast-2 = aws.ap-northeast-2
-    aws.ap-northeast-3 = aws.ap-northeast-3
-    aws.ap-south-1     = aws.ap-south-1
-    aws.ap-southeast-1 = aws.ap-southeast-1
-    aws.ap-southeast-2 = aws.ap-southeast-2
-    aws.ca-central-1   = aws.ca-central-1
-    aws.eu-central-1   = aws.eu-central-1
-    aws.eu-north-1     = aws.eu-north-1
-    aws.eu-west-1      = aws.eu-west-1
-    aws.eu-west-2      = aws.eu-west-2
-    aws.eu-west-3      = aws.eu-west-3
-    aws.sa-east-1      = aws.sa-east-1
-    aws.us-east-1      = aws.us-east-1
-    aws.us-east-2      = aws.us-east-2
-    aws.us-west-1      = aws.us-west-1
-    aws.us-west-2      = aws.us-west-2
-  }
-}
+#   providers = {
+#     aws                = aws
+#     aws.ap-northeast-1 = aws.ap-northeast-1
+#     aws.ap-northeast-2 = aws.ap-northeast-2
+#     aws.ap-northeast-3 = aws.ap-northeast-3
+#     aws.ap-south-1     = aws.ap-south-1
+#     aws.ap-southeast-1 = aws.ap-southeast-1
+#     aws.ap-southeast-2 = aws.ap-southeast-2
+#     aws.ca-central-1   = aws.ca-central-1
+#     aws.eu-central-1   = aws.eu-central-1
+#     aws.eu-north-1     = aws.eu-north-1
+#     aws.eu-west-1      = aws.eu-west-1
+#     aws.eu-west-2      = aws.eu-west-2
+#     aws.eu-west-3      = aws.eu-west-3
+#     aws.sa-east-1      = aws.sa-east-1
+#     aws.us-east-1      = aws.us-east-1
+#     aws.us-east-2      = aws.us-east-2
+#     aws.us-west-1      = aws.us-west-1
+#     aws.us-west-2      = aws.us-west-2
+#   }
+# }
 
 module "aws-alternate-contacts" {
   source = "./aws-alternate-contacts"
@@ -35,19 +35,19 @@ module "aws-alternate-contacts" {
     aws = aws
   }
 
-  operations_email_address = ""
-  operations_name = ""
-  operations_phone_number = ""
+  operations_email_address = var.operations_email_address != null ? var.operations_email_address : "operations@example.com"
+  operations_name = "Operations"
+  operations_phone_number = var.operations_phone_number != null ? var.operations_phone_number : ""
   operations_title = ""
 
-  billing_email_address = ""
-  billing_name = ""
-  billing_phone_number = ""
+  billing_email_address = var.billing_email_address != null ? var.billing_email_address : "billing@example.com"
+  billing_name = "Billing"
+  billing_phone_number = var.billing_phone_number != null ? var.billing_phone_number : ""
   billing_title = ""
 
-  security_email_address = ""
-  security_name = ""
-  security_phone_number = ""
+  security_email_address = var.security_email_address != null ? var.security_email_address : "security@example.com"
+  security_name = "Security"
+  security_phone_number = var.security_phone_number != null ? var.security_phone_number : ""
   security_title = ""
   
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,49 @@
-variable "aws_secure_baseline_enabled" {
-  description = "Enable secure baseline configuration for an AWS account"
-  default = false
-}
+# variable "aws_secure_baseline_enabled" {
+#   description = "Enable secure baseline configuration for an AWS account"
+#   default = false
+# }
 
 variable "aws_alternate_contacts_enabled" {
   description = "Enable alternate contacts configuration for an AWS account"
   default = false
 }
 
-variable "aws_support_roles" {
-  description = "A list of IAM roles for operations support"
-  default = []
+variable "operations_email_address" {
+  description = "Email for operations notifications"
+  default = null
 }
 
-variable "aws_audit_log_bucket" {
-    description = "Name of an S3 bucket used for audit logs"
-    default = "audit_logs"
+variable "operations_phone_number" {
+  description = "Phone number for operations notifications"
+  default = null
 }
+
+variable "security_email_address" {
+  description = "Email for security notifications"
+  default = null
+}
+
+variable "security_phone_number" {
+  description = "Phone number for security notifications"
+  default = null
+}
+
+variable "billing_email_address" {
+  description = "Email for billing notifications"
+  default = null
+}
+
+variable "billing_phone_number" {
+  description = "Phone number for billing notifications"
+  default = null
+}
+
+# variable "aws_support_roles" {
+#   description = "A list of IAM roles for operations support"
+#   default = []
+# }
+
+# variable "aws_audit_log_bucket" {
+#     description = "Name of an S3 bucket used for audit logs"
+#     default = "audit_logs"
+# }
